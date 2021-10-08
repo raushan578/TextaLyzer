0aa6defa5bf031855b5e898a3f78ef4833c41465
Raushan Raj <80462898+raushan578@users.noreply.github.com>
2021-10-08 18:52:04 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Textform.js'
+import './Textform'
 import './Navbar.css' 
 export default function Navbar(props) {
     return (<>
